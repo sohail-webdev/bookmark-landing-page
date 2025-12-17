12e41459051fd61d801a8f4e17550fc4f08aa9a0
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-17 21:49:37 +0530

    feat: validate email input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,31 @@
+import { useCallback, useRef } from "react";
 export default function Contact() {
+  const input_ref = useRef(null);
+  const report_input_validity = useCallback(() => {
+    if (!input_ref.current) return;
+    if (input_ref.current.checkValidity()) input_ref.current.classList.remove("invalid");
+    else input_ref.current.classList.add("invalid");
+  }, []);
+
   return (
     <section className="section section-contact">
       <p>35,000+ already joined</p>
       <h2>Stay up-to-date with what we’re doing</h2>
       <div className="contact-us-container">
-        <input type="email" name="email" placeholder="Enter your email address" className="text-input" />
-        <button className="button secondary-button">Contact Us</button>
+        <div className="email-input-wrapper">
+          <input
+            ref={input_ref}
+            type="email"
+            name="email"
+            placeholder="Enter your email address"
+            className="text-input"
+            onInput={event => (event.target.classList.contains("invalid") ? report_input_validity() : null)}
+          />
+          <p>Whoops, make sure it's an email</p>
+        </div>
+        <button className="button secondary-button" onClick={report_input_validity}>
+          Contact Us
+        </button>
       </div>
     </section>
   );
